ab69d1d884eaa45d97e476c95334af87c45d6e86
shivangi-gupta-01 <110471751+shivangi-gupta-01@users.noreply.github.com>
2024-10-21 23:25:30 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import About from "@/sections/About"
+import Features from "@/sections/Features"
+import Hero from "@/sections/Hero"
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <About />
+    </>
+  )
+}
+
+export default Home
